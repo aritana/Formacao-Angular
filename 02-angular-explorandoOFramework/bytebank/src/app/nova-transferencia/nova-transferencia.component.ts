e02eed8c38a8e99a65f2c86480e5c5c7e117b3a7
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-01-31 13:24:00 -0300

    extract with formatted date and currency

--- 02-angular-explorandoOFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ 02-angular-explorandoOFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-  valor: number = 1;
-  destino: number = 2;
+  @Output() aoTansferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
   transferir() {
     console.log('solicitado nova transferencia');
-    console.log(this.valor);
-    console.log(this.destino);
+
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTansferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
