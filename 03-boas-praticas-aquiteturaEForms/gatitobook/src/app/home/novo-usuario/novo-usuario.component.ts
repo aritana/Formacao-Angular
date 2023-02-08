6316d2a687e31d6aa7ab5423bba4a805852df264
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-02-08 00:26:52 -0300

    dynamic validation

--- 03-boas-praticas-aquiteturaEForms/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ 03-boas-praticas-aquiteturaEForms/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -13,14 +14,19 @@ export class NovoUsuarioComponent implements OnInit {
   novoUsuarioForm!: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private UsuarioExisteService: UsuarioExisteService
   ) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
-      userName: ['',[minusculoValidator]],
+      userName: [
+        '',
+        [minusculoValidator],
+        [this.UsuarioExisteService.usuarioJaExiste()],
+      ],
       password: [''],
     });
   }
--- 03-boas-praticas-aquiteturaEForms/gatitobook/src/app/home/novo-usuario/usuario-existe.service.ts
+++ 03-boas-praticas-aquiteturaEForms/gatitobook/src/app/home/novo-usuario/usuario-existe.service.ts
@@ -1,9 +1,30 @@
+import { AbstractControl } from '@angular/forms';
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Injectable } from '@angular/core';
+import { first, map, switchMap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioExisteService {
-
-  constructor() { }
+  constructor(private novoUsuarioService: NovoUsuarioService) {}
+  usuarioJaExiste() {
+    /*observable, assincrono. cada digitacao
+    o angular observa, converter a digitacao em requisicao, value changes,
+    pipe, do rxjs*/
+    return (control: AbstractControl) => {
+      //recebe o que o usuário tecla
+      return control.valueChanges.pipe(
+        //fazer troca do fluxo da digitação pelo da requisição.
+        switchMap(
+          (nomeUsuario) =>
+            this.novoUsuarioService.verificaUsuarioExistente(nomeUsuario) //true or false
+        ),
+        map(
+          (usuarioExiste) => (usuarioExiste ? { usuarioExistente: true } : null) //objeto ou nulo
+        ),
+        first() //Emit the first value or first to pass provided expression, encerra
+      );
+    };
+  }
 }
